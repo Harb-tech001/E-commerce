6b427304806796c31cade624708a630b3c337737
Harb-tech001 <harbtech01@gmail.com>
2025-09-17 02:34:07 +1300

    first commit

--- src/script.js
+++ src/script.js
@@ -6,7 +6,7 @@ const desserts = [
       thumbnail: "./assets/images/image-waffle-thumbnail.jpg",
       mobile: "./assets/images/image-waffle-mobile.jpg",
       tablet: "./assets/images/image-waffle-tablet.jpg",
-      desktop: "../images/image-waffle-desktop.jpg",
+      desktop: "./images/image-waffle-desktop.jpg",
     },
     category: "Waffle",
     name: "Waffle with Berries",
@@ -19,7 +19,7 @@ const desserts = [
       thumbnail: "./assets/images/image-creme-brulee-thumbnail.jpg",
       mobile: "./assets/images/image-creme-brulee-mobile.jpg",
       tablet: "./assets/images/image-creme-brulee-tablet.jpg",
-      desktop: "../images/image-creme-brulee-desktop.jpg",
+      desktop: "./images/image-creme-brulee-desktop.jpg",
     },
     category: "Crème Brûlée",
     name: "Vanilla Bean Crème Brûlée",
@@ -32,7 +32,7 @@ const desserts = [
       thumbnail: "./assets/images/image-macaron-thumbnail.jpg",
       mobile: "./assets/images/image-macaron-mobile.jpg",
       tablet: "./assets/images/image-macaron-tablet.jpg",
-      desktop: "../images/image-macaron-desktop.jpg",
+      desktop: "./images/image-macaron-desktop.jpg",
     },
     category: "Macaron",
     name: "Macaron Mix of Five",
@@ -45,7 +45,7 @@ const desserts = [
       thumbnail: "./assets/images/image-tiramisu-thumbnail.jpg",
       mobile: "./assets/images/image-tiramisu-mobile.jpg",
       tablet: "./assets/images/image-tiramisu-tablet.jpg",
-      desktop: "../images/image-tiramisu-desktop.jpg",
+      desktop: "./images/image-tiramisu-desktop.jpg",
     },
     category: "Tiramisu",
     name: "Classic Tiramisu",
@@ -58,7 +58,7 @@ const desserts = [
       thumbnail: "./assets/images/image-baklava-thumbnail.jpg",
       mobile: "./assets/images/image-baklava-mobile.jpg",
       tablet: "./assets/images/image-baklava-tablet.jpg",
-      desktop: "../images/image-baklava-desktop.jpg",
+      desktop: "./images/image-baklava-desktop.jpg",
     },
     category: "Baklava",
     name: "Pistachio Baklava",
@@ -71,7 +71,7 @@ const desserts = [
       thumbnail: "./assets/images/image-meringue-thumbnail.jpg",
       mobile: "./assets/images/image-meringue-mobile.jpg",
       tablet: "./assets/images/image-meringue-tablet.jpg",
-      desktop: "../images/image-meringue-desktop.jpg",
+      desktop: "./images/image-meringue-desktop.jpg",
     },
     category: "Pie",
     name: "Lemon Meringue Pie",
@@ -84,7 +84,7 @@ const desserts = [
       thumbnail: "./assets/images/image-cake-thumbnail.jpg",
       mobile: "./assets/images/image-cake-mobile.jpg",
       tablet: "./assets/images/image-cake-tablet.jpg",
-      desktop: "../images/image-cake-desktop.jpg",
+      desktop: "./images/image-cake-desktop.jpg",
     },
     category: "Cake",
     name: "Red Velvet Cake",
@@ -97,7 +97,7 @@ const desserts = [
       thumbnail: "./assets/images/image-brownie-thumbnail.jpg",
       mobile: "./assets/images/image-brownie-mobile.jpg",
       tablet: "./assets/images/image-brownie-tablet.jpg",
-      desktop: "../images/image-brownie-desktop.jpg",
+      desktop: "./images/image-brownie-desktop.jpg",
     },
     category: "Brownie",
     name: "Salted Caramel Brownie",
@@ -110,7 +110,7 @@ const desserts = [
       thumbnail: "./assets/images/image-panna-cotta-thumbnail.jpg",
       mobile: "./assets/images/image-panna-cotta-mobile.jpg",
       tablet: "./assets/images/image-panna-cotta-tablet.jpg",
-      desktop: "../images/image-panna-cotta-desktop.jpg",
+      desktop: "./images/image-panna-cotta-desktop.jpg",
     },
     category: "Panna Cotta",
     name: "Vanilla Panna Cotta",
@@ -136,14 +136,14 @@ function renderMenu() {
       cartItem ? "outline-3 outline-[#be4324]" : ""} ">
       ${cartItem
         ? `<button class="absolute bottom-[-20px] left-[50px] lg:left-[55px] flex space-x-4 lg:space-x-8  bg-[#be4324] px-2 lg:px-4 py-1 lg:py-2 rounded-full">
-              <img onclick="updateQty(${item.id}, -1)" src=${"../images/icon-decrement-quantity.svg"} class="border border-[#ad8985] rounded-[100%] lg:p-1.5 p-1">
+              <img onclick="updateQty(${item.id}, -1)" src=${"./images/icon-decrement-quantity.svg"} class="border border-[#ad8985] rounded-[100%] lg:p-1.5 p-1">
               <span class="font-semibold text-white">${cartItem.qty}</span>
               <img onclick="updateQty(${
                 item.id
-              }, 1)" src=${"../images/icon-increment-quantity.svg"} class="border border-[#ad8985] rounded-[100%] lg:p-1.5 p-1">
+              }, 1)" src=${"./images/icon-increment-quantity.svg"} class="border border-[#ad8985] rounded-[100%] lg:p-1.5 p-1">
            </button>`
         : `<button onclick="addToCart(${item.id})" class="absolute bottom-[-20px] left-[50px] lg:left-[55px] flex space-x-2 bg-white text-black border border-gray-400 px-6 py-2 rounded-[20px]">
-              <img src=${"../images/icon-add-to-cart.svg"} alt="cart-img"> 
+              <img src=${"./images/icon-add-to-cart.svg"} alt="cart-img"> 
               <span class="font-medium">Add to Cart</span>
            </button>`
     }
@@ -182,7 +182,7 @@ function renderCart() {
     // only show empty message
     itemsDiv.innerHTML = `
       <div class="flex flex-col items-center">
-      <img src=${"../images/illustration-empty-cart.svg"}>
+      <img src=${"./images/illustration-empty-cart.svg"}>
       <p class="text-gray-500 text-sm">Your added items will appear here.</p>
       </div>
       `;
@@ -227,7 +227,7 @@ function renderCart() {
       <span class="font-bold text-[25px]">$${total.toFixed(2)}</span>
     </div>
     <div class="bg-rose-50 flex items-center justify-center space-x-2.5 w-[16rem] lg:w-[15rem] h-[5rem] lg:h-[20rem] py-2 px-2 ml-[30px] mt-5 rounded-[5px]"> 
-        <img src=${"../images/icon-carbon-neutral.svg"}>
+        <img src=${"./images/icon-carbon-neutral.svg"}>
         <p class=" text-sm text-[#867472] text-center">This is a <span class="font-medium"> carbon-neutral </span> delivery</p>
     </div>
        <button onclick="openCheckoutModal()" class="bg-red-700 hover:bg-red-800 text-white w-[16rem] lg:w-[15rem] h-[3rem] py-2 mt-4 ml-[30px] rounded-[25px] font-semibold">
@@ -274,18 +274,12 @@ function removeFromCart(id) {
   renderCart();
   renderMenu();
 }
-
 // Initialize
 renderMenu();
 renderCart();
-
-// !!!!!!!!!!!!!!!!!!!!!!!!
-// MODAL PART
-
 // Open Checkout Modal (creates modal in JS)
 function openCheckoutModal() {
   let total = 0;
-
   // Build checkout items list
   let itemsHTML = "";
   cart.forEach((item) => {
@@ -294,8 +288,7 @@ function openCheckoutModal() {
       <div class="flex justify-between items-center border-t-gray-300">
         <div class="flex items-center space-x-3">
           <img src="${item.image}" alt="${
-      item.name
-    }" class="w-12 h-12 rounded-lg object-cover">
+      item.name}" class="w-12 h-12 rounded-lg object-cover">
           <div class="text-left">
             <p class="font-semibold text-gray-800">${item.name}</p>
             <p class="text-md text-gray-500"><span class="text-red-800 text-lg font-medium"> ${
@@ -318,15 +311,14 @@ function openCheckoutModal() {
   modal.id = "checkout-modal";
   modal.className =
     "fixed inset-0 bg-opacity-40 flex items-center justify-center z-50 mt-[40px]";
-
   modal.innerHTML = `
     <div class="bg-white rounded-2xl shadow-md w-[30rem] p-8 relative">
       <button id="close-modal" class="absolute top-3 right-3">
-        <img src="/icons/remove-item.svg" class="w-[12px] h-[15px]">
+        <img src=${"./images/icons/remove-item.svg"} class="w-[12px] h-[15px]">
       </button>
 
         <div class="">
-          <img src="/icons/success-check.svg" alt="success" class="w-10 h-10 mb-3">
+          <img src=${"./images/icons/success-check.svg"} alt="success" class="w-10 h-10 mb-3">
           <h2 class="text-2xl font-bold text-gray-900 mb-2">Order Confirmed</h2>
           <p class="text-gray-500 mb-6">We hope you enjoy your food!</p>
         </div>
